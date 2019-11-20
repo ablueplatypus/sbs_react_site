c861214045de66e0c8bf58fe676d4ae7f9ee910f
Matthew Doering <mattdoering86@gmail.com>
2019-11-20 01:14:17 -0500

    added an about Page and linked page with Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 // import React from 'react';
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import About from './pages/About';
 
 const App = () => {
 
   return (
     <Router>
         <Route exact path="/" component={Home}/>
+        <Route path="/about" component={About}/>
     </Router>
   );
 }
---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Navbar from '../componets/Navbar';
+
+const About = (props) => {
+  return (
+    <div>
+    <Navbar/>
+      <h1>About Me</h1>
+    </div>
+  )
+
+}
+
+export default About;
